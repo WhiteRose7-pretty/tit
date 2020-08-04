b5ccc96d8f2f66b657eceb4868360c7de7d5c34b
TymoteuszGraham <biuro@tgcoders.pl>
2020-08-04 10:40:49 +0200

    added verbose name o and options to upload file

--- app/models.py
+++ app/models.py
@@ -3,14 +3,28 @@ from ckeditor.fields import RichTextField
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
-    menu = models.BooleanField(default=False)
+    name = models.CharField(max_length=50, verbose_name='Nazwa kategorii')
+    menu = models.BooleanField(default=False, verbose_name='Pokaż w menu')
+
+    class Meta:
+        verbose_name = 'Kategorii artykułów'
+        verbose_name_plural = 'Kategorii artykułów'
+
+    def __str__(self):
+        return self.name
 
 
 class Article(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    img = models.ImageField()
-    date_of_publication = models.DateTimeField()
-    content = RichTextField()
-    premium = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Kategoria artykułu')
+    title = models.CharField(max_length=100, verbose_name='Tytuł artykułu')
+    img = models.ImageField(verbose_name='Zdjęcie')
+    date_of_publication = models.DateTimeField(verbose_name='Data publikacji')
+    content = RichTextField(verbose_name='Treść artykułu')
+    premium = models.BooleanField(default=True, verbose_name='Widoczne tylko dla PREMIUM')
+
+    class Meta:
+        verbose_name = 'Artykuły'
+        verbose_name_plural = 'Artykuły'
+
+    def __str__(self):
+        return self.title
